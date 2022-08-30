80cbc9361ad4527335ab046e18509013a702e4fb
vitcas <anonfansub@gmail.com>
2022-08-30 16:45:34 -0300

    fixed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,118 @@ namespace RuneChan
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ARAM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SUMO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "TROLL";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(16, 128);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "RANDOM";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Patch:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(88, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(211, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(227, 165);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "runechan v1.0";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,232 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RuneChan
 {
     public partial class Form1 : Form
     {
+        public class Rune
+        {
+            public List<object> autoModifiedSelections { get; set; }
+            public bool current { get; set; }
+            public int id { get; set; }
+            public bool isActive { get; set; }
+            public bool isDeletable { get; set; }
+            public bool isEditable { get; set; }
+            public bool isValid { get; set; }
+            public long lastModified { get; set; }
+            public string name { get; set; }
+            public int order { get; set; }
+            public int primaryStyleId { get; set; }
+            public List<int> selectedPerkIds { get; set; }
+            public int subStyleId { get; set; }
+        }
+        public string port;
+        public string b64;
+        public int xamp;
+        public string runa;
+        public string acid;
+        public List<Rune> runesAram;
+        public List<Rune> runesSr;
+        public bool AcceptAllCertifications(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certification, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+        public void Auth()
+        {
+            System.Diagnostics.Process.Start("auth.bat");
+            System.Threading.Thread.Sleep(2000);
+            string[] arrLine = File.ReadAllLines("auth.txt");
+            string token = arrLine[0].Remove(arrLine[0].Length - 1);
+            port = arrLine[1].Remove(arrLine[1].Length - 1);
+            b64 = EncodeTo64("riot:" + token);
+            File.Delete("auth.txt");
+            File.Delete("temp.txt");
+            File.Delete("temp2.txt");
+            File.Delete("temp3.txt");
+        }
+        public string EncodeTo64(string toEncode)
+        {
+            byte[] toEncodeAsBytes = Encoding.ASCII.GetBytes(toEncode);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+        public void LoadJson()
+        {
+            var webRequesta = WebRequest.Create(@"https://pastebin.com/raw/5WBtRahe");
+            using (var response = webRequesta.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                var strContent = reader.ReadToEnd();
+                runesAram = JsonConvert.DeserializeObject<List<Rune>>(strContent);
+            }
+            var webRequestb = WebRequest.Create(@"https://pastebin.com/raw/dR5vE4Ba");
+            using (var response = webRequestb.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                var strContent = reader.ReadToEnd();
+                runesSr = JsonConvert.DeserializeObject<List<Rune>>(strContent);
+            }
+            var webRequestc = WebRequest.Create(@"https://pastebin.com/raw/kcycDGYk");
+            using (var response = webRequestc.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                var strContent = reader.ReadToEnd();
+                label2.Text = strContent + "\n";
+            }
+
+        }
+        public string BuscaA(int campeao)
+        {
+            foreach (var item in runesAram)
+            {
+                if (item.id == campeao)
+                {
+                    runa = JsonConvert.SerializeObject(item);
+                }
+            }
+            return runa.ToString();
+        }
+        public string BuscaS(int campeao)
+        {
+            foreach (var item in runesSr)
+            {
+                if (item.id == campeao)
+                {
+                    runa = JsonConvert.SerializeObject(item);
+                }
+            }
+            return runa.ToString();
+        }
+        public void Post()
+        {
+            string uri = "https://127.0.0.1:" + port + "/lol-perks/v1/pages";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.PreAuthenticate = true;
+            httpWebRequest.Headers.Add("Authorization", "Basic " + b64);
+            httpWebRequest.Accept = "application/json";
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(runa);
+            }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+        public string Get(string api)
+        {
+            string uri = "https://127.0.0.1:" + port + api;       
+            string result;
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.PreAuthenticate = true;
+            httpWebRequest.Headers.Add("Authorization", "Basic " + b64);
+            httpWebRequest.Accept = "application/json";
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            return result;
+        }
+        public void Del()
+        {
+            string uri = "https://127.0.0.1:" + port + "/lol-perks/v1/pages";
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.PreAuthenticate = true;
+            httpWebRequest.Headers.Add("Authorization", "Basic " + b64);
+            httpWebRequest.Accept = "application/json";
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "DELETE";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(510);
+            }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+            LoadJson();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback = (snder, cert, chain, error) => true;
+            Auth();
+            try {
+                dynamic stuff = JsonConvert.DeserializeObject(Get("/lol-summoner/v1/current-summoner"));
+                acid = stuff.accountId;
+            }    
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Application.Exit();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dynamic lobby = JsonConvert.DeserializeObject(Get("/lol-lobby-team-builder/champ-select/v1/session"));
+            foreach (dynamic player in lobby.myTeam)
+            {
+                if (player.summonerId == acid)
+                {
+                    xamp = player.championId;
+                    break;
+                }
+            }
+            BuscaA(xamp);
+            Del();
+            Post();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dynamic lobby = JsonConvert.DeserializeObject(Get("/lol-lobby-team-builder/champ-select/v1/session"));
+            foreach (dynamic player in lobby.myTeam)
+            {
+                if (player.summonerId == acid)
+                {
+                    xamp = player.championId;
+                    break;
+                }
+            }
+            BuscaS(xamp);
+            Del();
+            Post();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            //button4.Enabled = false;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            BuscaA(887);
+            Post();
+        }
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            BuscaA(666);
+            Post();
         }
     }
 }
